2d774a84d6b050d830f63dcdc1c587c220422eb6
Dogcatfee <msessions21@gmail.com>
2019-01-18 20:50:40 -0800

    Update sign_kernel_full.sh

--- sign_kernel_full.sh
+++ sign_kernel_full.sh
@@ -8,7 +8,7 @@ mkimage -D "-I dts -O dtb -p 2048" -f kernel.its vmlinux.uimg
 echo "(Stage 2) Bootloader file"
 dd if=/dev/zero of=bootloader.bin bs=512 count=1
 
-# Sign kernel and write to bootloader file
+# Sign and pack boot image
 echo "(Stage 3) Sign Kernel to file"
 vbutil_kernel \
     --pack vmlinux.kpart \
@@ -20,6 +20,6 @@ vbutil_kernel \
     --config cmdline \
     --bootloader bootloader.bin
 
-# Write bootloader to disk, change null to partition.
+# Write boot image to boot partition, change null to partition.
 echo "(Stage 4) Write bootloader to disk"
 dd if=vmlinux.kpart of=/dev/null
